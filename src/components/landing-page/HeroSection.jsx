724f52afaa5573a6c53eb4a6bb107ef9c3dfbf87
aditya-pushkar <adityapushkar850@gmail.com>
2023-03-26 16:47:39 +0530

    ui refactoring

--- src/components/landing-page/HeroSection.jsx
+++ src/components/landing-page/HeroSection.jsx
@@ -92,7 +92,7 @@ const HeroSection = () => {
           </div>
         </div>
       </nav>
-      <div className="relative bg-gray-300">
+      <div className="relative bg-gray-900">
         <div className="container m-auto px-6 pt-28 md:px-12 lg:pt-[1.8rem] lg:px-7">
           <div className="flex items-center flex-wrap px-2 md:px-0">
             <div className="relative lg:w-6/12 lg:py-24 xl:py-32">
